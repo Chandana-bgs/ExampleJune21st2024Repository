0c1ea10892f82893e370150f16e1da223e94ea3c
chandu <chanadana03052000@gmail.com>
2024-08-13 10:45:01 +0530

    p1

---
+++ program/p1.java
@@ -0,0 +1,12 @@
+package program;
+
+public class p1 {
+    public static void main(String[] args) {
+        int num=4;
+        int k=1;
+        for(int i=1;i<=num;i++){
+            k=k*i;
+        }
+        System.out.println(k);
+    }
+}
